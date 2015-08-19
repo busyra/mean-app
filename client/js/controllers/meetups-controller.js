cff0f02fd7379aa2da58e40050363c777f1d991c
Busyra Anwar <busyra@me.com>
2015-08-18 23:22:55 -0700

    adds app.js and reformats controller

--- client/js/controllers/meetups-controller.js
+++ client/js/controllers/meetups-controller.js
@@ -1,8 +1,12 @@
-function meetupsController($scope) {
-  $scope.meetupsCount = 10;
-  $scope.meetups=[
-    { name: "MEET UP 1"},
-    { name: "MEET UP 2"},
-    { name: "MEET UP 3"}
-  ]
-}
+app.controller('meetupsController',['$scope', '$resource',
+  function($scope, $resource){
+    $scope.meetups=[
+      { name: "MEET UP 1"},
+      { name: "MEET UP 2"},
+      { name: "MEET UP 3"}
+    ]
+    $scope.createMeetup = function(){
+      $scope.meetups.push({ name: $scope.meetupName });
+      $scope.meetupName ='';
+    }
+}]);

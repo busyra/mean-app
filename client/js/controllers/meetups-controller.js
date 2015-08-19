fa276e79087d62aee804fd96ec307896c4962d6f
Busyra Anwar <busyra@me.com>
2015-08-18 23:27:54 -0700

    updates add function to add to server

--- client/js/controllers/meetups-controller.js
+++ client/js/controllers/meetups-controller.js
@@ -1,12 +1,15 @@
 app.controller('meetupsController',['$scope', '$resource',
   function($scope, $resource){
+    var Meetup = $resource('/api/meetups');
+
     $scope.meetups=[
       { name: "MEET UP 1"},
       { name: "MEET UP 2"},
       { name: "MEET UP 3"}
     ]
     $scope.createMeetup = function(){
-      $scope.meetups.push({ name: $scope.meetupName });
-      $scope.meetupName ='';
+      var meetup = new Meetup();
+      meetup.name = $scope.meetupName;
+      meetup.$save();
     }
 }]);

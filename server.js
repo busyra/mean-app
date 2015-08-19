18e6a426c3699c543e84a6a9abeda617ad437d0f
Busyra Anwar <busyra@me.com>
2015-08-18 22:54:19 -0700

    updates routes to shortcut /client/js

--- server.js
+++ server.js
@@ -3,9 +3,11 @@ var express = require('express'),
     app     = express();
 //.get http verb
 app.get('/', function(req, res){
-//sends them to this page 
+//sends them to this page
   res.sendfile(__dirname + '/client/views/index.html');
 });
+
+app.use('/js', express.static(__dirname + '/client/js'));
 
 app.listen(3000, function(){
   console.log('I\'m listening...');

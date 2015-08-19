e071b257fcc50901bfcd0245ed27cb30b1f22f16
Busyra Anwar <busyra@me.com>
2015-08-18 22:28:32 -0700

    adds express

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
 //console.log('Hello from node');
+var express = require('express'),
+    app     = express ();
+
+app.get('/', function(req, res){
+  res.sendfile(__dirname + '/client/views/index.html');
+});
+
+app.listen(3000, function(){
+  console.log('I\'m listening...');
+})

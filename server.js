94710a6fdcd9aa6b5ae8ed7566ee89b6fc99d992
Busyra Anwar <busyra@me.com>
2015-08-18 22:36:57 -0700

    adds index page

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 //console.log('Hello from node');
 var express = require('express'),
-    app     = express ();
-
+    app     = express();
+//.get http verb
 app.get('/', function(req, res){
+//sends them to this page 
   res.sendfile(__dirname + '/client/views/index.html');
 });
 

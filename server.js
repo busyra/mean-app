75f1b6a46237a596d0198591d7b9059c7bce02ba
Busyra Anwar <busyra@me.com>
2015-08-19 00:05:11 -0700

    adds mongoose now saves to mongoDB

--- server.js
+++ server.js
@@ -2,7 +2,10 @@
 var express           = require('express'),
     app               = express();
     bodyParser        = require('body-parser'),
+    mongoose          = require('mongoose'),
     meetupsController = require('./server/controllers/meetups-controller');
+
+mongoose.connect('mongodb://localhost:27017/mean-demo');
 
 app.use(bodyParser());
 
